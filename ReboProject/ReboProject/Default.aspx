995ccb0b5dedd9762ef9abafda1dd61bb8fbb8fc
shubham <shubham.gupta@maxval.com>
2018-04-18 12:31:40 +0530

    logic for first datapoint

---
+++ ReboProject/ReboProject/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="Home Page" Language="C#"  AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReboProject._Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+	<title>Accepted Result</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+	<form id="form1" runat="server">
+	    <table class="table table-bordered">
+        <thead>
+            <tr class="success">
+                <th>
+                    Accepted Result<br /><br />
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                     <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem Text="Admin management fee" Value="0" />
+                        <asp:ListItem Text="Security deposit" Value="1" />
+                        <asp:ListItem Text="Alteration" Value="2" />
+                        <asp:ListItem Text="Assign sublet" Value="3" />
+                        <asp:ListItem Text="Holdover" Value="4" />
+                        <asp:ListItem Text="Tenant audit" Value="5" />
+                        <asp:ListItem Text="Tenent sublet" Value="6" />
+                        <asp:ListItem Text="Late fee" Value="7" />
+                        <asp:ListItem Text="Permitted use" Value="8" />
+                        <asp:ListItem Text="Restoration surrender" Value="9" />
+                    </asp:DropDownList><br /><br />
+                    <input type="text"  class="input-text" placeholder="Enter text" style="width:500px;" runat="server" id="myTextBox" /><br /><br />
+					<asp:Button ID="Button1" runat="server" OnClick="TestClick" Text="Button" />
+                     <br /><br />
+                    <input type="text"  class="input-text" placeholder="Output" style="width:500px;" runat="server" id="Text1" /><br /><br />
+                
+                </td>
+            </tr>
+        </tbody>
+    </table>
+        
+    </form>
+</body>
+</html>
+<script>
+
+    
+    
+</script>
---
+++ ReboProject/ReboProject/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace ReboProject
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void TestClick(object sender, EventArgs e)
+        {
+            string val = DropDownList1.SelectedValue; // get the datapoint 
+            var accptedValThere = 0;
+            var getVerbiage = myTextBox.Value; // Verbiage in which data to search
+            var textLength =getVerbiage.Length; // get the length of the text
+            var endIndex =0;
+            Dictionary<string, string> searchField = new Dictionary<string, string>();
+            if (val == "0")
+            { // if Admin management fee
+
+                // get all the found text in dictionary
+                searchField.Add("administration fee", null);
+                searchField.Add("management fee", null);
+                searchField.Add("administrative cost", null);
+                searchField.Add("management cost", null);
+                searchField.Add("overhead charges", null);
+                searchField.Add("administrative services", null);
+                searchField.Add("management services", null);
+                searchField.Add("administrative expenses", null);
+                searchField.Add("management expenses", null);
+                searchField.Add("cost of management", null);
+                searchField.Add("cost of administration", null);
+                searchField.Add("cost of supervision", null);
+
+                foreach (KeyValuePair<string, string> entry in searchField)
+                {
+                    var searchFieldVal = entry.Key; // get the first search field
+                    var matchData = Regex.Matches(getVerbiage, @"\b\s?" + searchFieldVal + "\\w*\\b"); // find match
+                    if (matchData.Count > 0) // if match there
+                    {
+                        accptedValThere = 1; // result found
+                        var startIndex = getVerbiage.IndexOf(searchFieldVal); // get the index of search field
+                        var getTheTextBeforeWord =getVerbiage.Substring(0, startIndex); // get the string before word to find the full stop
+                        var getTheFullStopBeforeWord =getTheTextBeforeWord.LastIndexOf('.'); // get the index of the full stop
+                        if (getTheFullStopBeforeWord == -1) // full stop not present
+                        {
+                            getTheFullStopBeforeWord = 0;
+                            endIndex = textLength;
+                        }
+                        else 
+                        {
+                            endIndex = textLength - getTheFullStopBeforeWord - 1; // get the char count after full stop
+                            getTheFullStopBeforeWord += 1;// to avoid full stop in the accepted result
+                        } 
+                        var getTheTextAfterFullStopTillEnd = getVerbiage.Substring(getTheFullStopBeforeWord, endIndex); // get the result from full stop to end of Verbiage text
+                        var getTheFullStopAfterWord =getTheTextAfterFullStopTillEnd.IndexOf('.');// get the full stop after found text
+                        if (getTheFullStopAfterWord == -1) // full stop not present
+                        {
+                            getTheFullStopAfterWord = endIndex; 
+                        }
+                        else
+                            getTheFullStopAfterWord += 1; // to get the last full stop in accepted string
+                        string sub = getTheTextAfterFullStopTillEnd.Substring(0, getTheFullStopAfterWord); // get the final accepted result 
+                        Text1.Value = sub; // set the value of accepted result
+                        break;
+                    }
+                }
+            }
+            if (accptedValThere == 0)
+            {
+                Text1.Value = "Lease is silent";
+            }
+            
+        }
+    }
+}
